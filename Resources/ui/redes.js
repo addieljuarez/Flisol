23945165d7e80d3007749902fdbb439675821979
Addiel <addieljuarez@gmail.com>
2012-04-21 20:56:12 -0500

    borrando retroceso

--- Resources/ui/redes.js
+++ Resources/ui/redes.js
@@ -7,8 +7,8 @@
 // 
 
 
-var winRedes = Titanium.UI.currentWindow;
-winRedes.backgroundColor='#000';
+tweet = Ti.UI.currentWindow;
+tweet.backgroundColor='#000';
 
 // var regresar = Ti.UI.createImageView({
 	// image:'../images/BARRITA.png',
@@ -33,7 +33,7 @@ var logoPantalla = Titanium.UI.createImageView({
 	top:'5dp',
 	//left:'40dp',
 });
-winRedes.add(logoPantalla);
+tweet.add(logoPantalla);
 
 // cuadro grande contenedor
 var cuadroPrincipal= Ti.UI.createView({
@@ -46,15 +46,108 @@ var cuadroPrincipal= Ti.UI.createView({
 	down:'25dp',
 	top : '50dp',
 });
-winRedes.add(cuadroPrincipal)
+tweet.add(cuadroPrincipal)
+
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-var imagenTwitter = Titanium.UI.createImageView({
-	image : '../images/twitter.png',
-	top:'10%',
-	left:'25%',
-	width:'80dp',
-	height:'100dp'
+var rows = [];
+
+var activityIndicator = Ti.UI.createActivityIndicator({
+	style : Titanium.UI.iPhone.ActivityIndicatorStyle.BIG
 });
-cuadroPrincipal.add(imagenTwitter);
+
+activityIndicator.show();
+
+var tableView = Ti.UI.createTableView({
+	//height : 440,
+	//width : 300,
+	//top : 10,
+	//left : 10,
+	left:'15dp',
+	right:'15dp',
+	down:'25dp',
+	top : '50dp',
+});
+
+// tableView.addEventListener("click", function(el) {
+// 
+// var detalle=	Ti.UI.createWindow({
+		// title : "Detalhe",
+		// url : "/ui/detalleTweet.js",
+		// tweet : el.rowData.tweet,
+		// user_name : el.rowData.user_name,
+		// backgroundColor : "#000",
+		// _parent : Titanium.UI.currentWindow,
+		// navGroup : tweet.navGroup,
+		// rootWindow : tweet.rootWindow,
+	// });
+	// tweet.navGroup.open(detalle);
+// });
+
+tweet.add(tableView);
+
+var loaderTwitterSearch = Ti.Network.createHTTPClient({
+	timeout : 30000
+});
+
+loaderTwitterSearch.open("GET", "http://search.twitter.com/search.json?q=flisol");
+
+loaderTwitterSearch.onload = function() {
+	var result = JSON.parse(this.responseText);
+	var tweets = result.results;
+	for(var i = 0; i < tweets.length; i++) {
+		var row = Ti.UI.createTableViewRow({
+			height : '70dp',
+			hasChild : true,
+			tweet : tweets[i].text,
+			user_name : tweets[i].from_user
+		});
+
+		var image = Ti.UI.createImageView({
+			image : tweets[i].profile_image_url,
+			left : '7dp',
+			top : '17dp',
+			bottom : '5dp',
+			width : '30dp',
+			height:'30dp',
+		});
+		row.add(image);
+
+		var name = Ti.UI.createLabel({
+			text : tweets[i].from_user,
+			font : {
+				fontSize : '8dp'
+			},
+			left : '40dp',
+			top : '5dp',
+			height : '15dp',
+			color:'#000',
+		});
+		row.add(name);
+
+		var tweet = Ti.UI.createLabel({
+			text : tweets[i].text,
+			left : '40dp',
+			right:'5dp',
+			font : {
+				fontSize : '8.5dp'
+			},
+			height : '20dp',
+			top : '18dp',
+			color:'#000'
+		});
+		row.add(tweet);
+
+		rows.push(row);
+	}
+	tableView.setData(rows);
+	activityIndicator.hide();
+}
+
+loaderTwitterSearch.send();
+
+
+
+
 
